b3f9b38cee7396890a6727f4acab086d57b37fc5
Ho Tuan Kiet <aiden.hokiet@gmail.com>
2023-12-12 18:00:38 +0700

    update type getPurchase api, add icon show pass for input type password

--- src/apis/purchases.api.ts
+++ src/apis/purchases.api.ts
@@ -1,11 +1,11 @@
-import { DeletePurchase, Purchase, PurchasesStatus } from 'src/types/Purchases.type'
+import { DeletePurchase, Purchase, PurchasesListStatus } from 'src/types/Purchases.type'
 import { SuccessRespone } from 'src/types/Util.type'
 import http from 'src/utils/http'
 
 const url = '/purchases'
 
 const purchasesApi = {
-  getPurchases: (params: { status: PurchasesStatus }) => {
+  getPurchases: (params: { status: PurchasesListStatus }) => {
     return http.get<SuccessRespone<Purchase[]>>(url, { params })
   },
   addToCart: (body: { product_id: string; buy_count: number }) => {
